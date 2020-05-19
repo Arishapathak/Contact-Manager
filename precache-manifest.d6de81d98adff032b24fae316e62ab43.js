d2cfaf44919e305f2c32f635d2e4f4adda9c8b0c
Arishapathak <kiwipathak@gmail.com>
2020-05-19 13:00:10 +0530

    Updates

---
+++ precache-manifest.d6de81d98adff032b24fae316e62ab43.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ee2a0aa1eb34a4e5d0346bcc1fc8bc3",
+    "url": "/Contact-Manager/index.html"
+  },
+  {
+    "revision": "dfc5ab5df2cc68173220",
+    "url": "/Contact-Manager/static/css/2.54eb4c0e.chunk.css"
+  },
+  {
+    "revision": "fa4de2a3bbcca91bda85",
+    "url": "/Contact-Manager/static/css/main.891316d9.chunk.css"
+  },
+  {
+    "revision": "dfc5ab5df2cc68173220",
+    "url": "/Contact-Manager/static/js/2.5a7ae306.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Contact-Manager/static/js/2.5a7ae306.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fa4de2a3bbcca91bda85",
+    "url": "/Contact-Manager/static/js/main.524a1d1f.chunk.js"
+  },
+  {
+    "revision": "c81e78001882d25cdb31",
+    "url": "/Contact-Manager/static/js/runtime-main.e84b6380.js"
+  }
+]);
